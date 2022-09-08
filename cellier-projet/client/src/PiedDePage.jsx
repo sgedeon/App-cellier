00bf22c672c394e5143c29ff8b9202bf7ca16118
EloiseEmery <e2195563@cmaisonneuve.qc.ca>
2022-09-08 19:30:04 -0400

    Correction du bug du footer

---
+++ cellier-projet/client/src/PiedDePage.jsx
@@ -0,0 +1,12 @@
+import './PiedDepage.scss';
+
+export default function PiedDepage(props) {
+
+	return (
+	<>
+		<div className="PiedDepage">
+	  		<small className="">© Mon Vino 2022, Tous droits réservés</small>
+		</div>
+	</>
+	);
+}

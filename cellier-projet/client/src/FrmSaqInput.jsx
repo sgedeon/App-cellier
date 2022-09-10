7f212a6f0f29b43f6a2d03e881bc14f8684fc2bd
David Deschenes <davids09@hotmail.com>
2022-09-09 22:36:52 -0400

    Gestion du changement de couleur des boutons select dans le menu admin et déplacement de la localisation du bouton admin dans l'interface à côté du bouton ajouter

--- cellier-projet/client/src/FrmSaqInput.jsx
+++ cellier-projet/client/src/FrmSaqInput.jsx
@@ -5,7 +5,7 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import { createTheme } from "@material-ui/core/styles";
+import { styled } from "@mui/system";
 
 export default function FrmSaqInput(props) {
   const gererType = (e) => {
@@ -20,7 +20,32 @@ export default function FrmSaqInput(props) {
   }
   return (
     <div className="FrmSaqInput">
-      <FormControl sx={{ m: 1, minWidth: 120, color: "red" }} size="small">
+      <FormControl
+        sx={[
+          {
+            m: 1,
+            minWidth: 120,
+          },
+          {
+            backgroundColor: "#FFFFFF",
+          },
+          {
+            "& label.Mui-focused": { color: "#f1ab50" },
+          },
+          {
+            "&:Mui-focused": {
+              backgroundColor: "#f1ab50",
+              color: "#f3f5eb",
+            },
+
+            "& .MuiInput-underline:after": {
+              borderBottomColor: "#f1ab50",
+            },
+          },
+        ]}
+        variant="standard"
+        size="small"
+      >
         <InputLabel id="nombre">Nombre</InputLabel>
         <Select
           labelId="nombre"
@@ -36,6 +61,8 @@ export default function FrmSaqInput(props) {
         </Select>
       </FormControl>
       <TextField
+        variant="standard"
+        s
         placeholder="Page"
         sx={{ m: 1, minWidth: 120 }}
         size="small"
@@ -47,7 +74,32 @@ export default function FrmSaqInput(props) {
         inputProps={{ min: 0 }}
         defaultValue={props.page}
       />
-      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <FormControl
+        variant="standard"
+        sx={[
+          {
+            m: 1,
+            minWidth: 120,
+          },
+          {
+            backgroundColor: "#FFFFFF",
+          },
+          {
+            "& label.Mui-focused": { color: "#f1ab50" },
+          },
+          {
+            "&:Mui-focused": {
+              backgroundColor: "#f1ab50",
+              color: "#f3f5eb",
+            },
+
+            "& .MuiInput-underline:after": {
+              borderBottomColor: "#f1ab50",
+            },
+          },
+        ]}
+        size="small"
+      >
         <InputLabel id="type">Type</InputLabel>
         <Select
           labelId="type"

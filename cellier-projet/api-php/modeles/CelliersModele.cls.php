0a7397588eb5ff0756aef563406486ed00126cf4
David Deschenes <davids09@hotmail.com>
2022-09-14 16:46:08 -0400

    Modifications au modèle celliers sur la requête remplacer

--- cellier-projet/api-php/modeles/CelliersModele.cls.php
+++ cellier-projet/api-php/modeles/CelliersModele.cls.php
@@ -23,9 +23,6 @@ class CelliersModele extends AccesBd
 
     public function remplacer($id, $cellier)
     {
-        return $this->modifier("UPDATE vino__cellier SET vino__cellier.nom=? WHERE id=?", [
-            $cellier->nom,
-            $id
-        ]);
+        return $this->modifier("UPDATE vino__cellier SET vino__cellier.nom=:  WHERE vino__cellier.id=:id", ['id' => $id["cellier"]]);
     }
 }

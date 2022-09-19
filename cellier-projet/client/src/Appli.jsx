fa17f0b6064caca59d24dd6a5863f5212ec3786b
Sebastien Gedeon <s.gedeon@hotmail.fr>
2022-09-19 14:13:40 -0400

    Test résolution bug refresh , si quelqu'un peut me confirmer que ca marche

--- cellier-projet/client/src/Appli.jsx
+++ cellier-projet/client/src/Appli.jsx
@@ -33,11 +33,7 @@ const Appli = () => {
   const [bouteilles, setBouteilles] = useState([]);
   const [emailUtilisateur, setEmailUtilisateur] = useState([]);
   const [id, setId] = useState([]);
-  const [cellier, setCellier] = useState(() => {
-    const savedCellier = localStorage.getItem("cellier");
-    const initialValueCellier = JSON.parse(savedCellier);
-    return initialValueCellier || "";
-  });
+  const [cellier, setCellier] = useState([]);
   const [username, setUsername] = useState([]);
   const [utilisateur, setUtilisateur] = useState([]);
   const [utilisateurs, setUtilisateurs] = useState([]);
@@ -69,6 +65,12 @@ const Appli = () => {
     createUser(emailUtilisateur);
     DATA = true;
   });
+
+  useEffect(() => {
+    setCellier(
+      JSON.parse(localStorage.getItem("cellier"))
+    )
+  },[]);
 
   useEffect(() => {
     fetchCelliers();

352f502d03f2e64c329d383b271decb013c6c9df
EloiseEmery <e2195563@cmaisonneuve.qc.ca>
2022-09-19 20:09:17 -0400

    Ajustement de l'entete des formulaire d'ajout cellier et bouteille

--- cellier-projet/client/src/FrmAjoutCellier.jsx
+++ cellier-projet/client/src/FrmAjoutCellier.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
+import { NavLink } from "react-router-dom";
+import rowIcone from "./img/svg/icone_row_left_white_filled.svg";
 
 function FormAjoutCellier({ celliers, URI, fetchCelliers, setError }) {
   const navigate = useNavigate();
@@ -67,36 +69,49 @@ function FormAjoutCellier({ celliers, URI, fetchCelliers, setError }) {
   }
 
   return (
-    <div className="FormAjoutCellier">
-		<h1>Ajouter un cellier</h1>
-		<div className="form-ajout--container">
-			<label htmlFor="nom">Nom du cellier</label>
-			<br></br>
-			<input type="text" id="nomCellier" name="nom" placeholder="Nom" />
-			<br></br>
-			<span></span>
-			<br></br>
-			<button onClick={gererAjoutCellier}>Ajouter</button>
+	<div>
+		<div className="Appli--entete">
+			<div>
+				<NavLink to={`/`}>
+					<button className="retour">
+					<img src={rowIcone} alt="icone-row-left" width={15}></img>Retour&nbsp;aux&nbsp;Celliers&nbsp;
+					</button>
+				</NavLink>
+			</div>
 		</div>
-	  	<Snackbar
-			sx={{ height: "70%" }}
-			anchorOrigin={{
-			vertical: "top",
-			horizontal: "center",
-			}}
-			open={openAlert}
-			autoHideDuration={2000}
-			onClose={handleCloseAlert}
-		>
-			<Alert
-			onClose={handleCloseAlert}
-			severity={severity}
-			sx={{ width: "100%" }}
-			>
-			{messageRetour}
-			</Alert>
-		</Snackbar>
-    </div>
+		<div className="Appli--container">
+			<div className="FormAjoutCellier">
+				<h1>Ajouter un cellier</h1>
+				<div className="form-ajout--container">
+					<label htmlFor="nom">Nom du cellier</label>
+					<br></br>
+					<input type="text" id="nomCellier" name="nom" placeholder="Nom" />
+					<br></br>
+					<span></span>
+					<br></br>
+					<button onClick={gererAjoutCellier}>Ajouter</button>
+				</div>
+				<Snackbar
+					sx={{ height: "70%" }}
+					anchorOrigin={{
+					vertical: "top",
+					horizontal: "center",
+					}}
+					open={openAlert}
+					autoHideDuration={2000}
+					onClose={handleCloseAlert}
+				>
+					<Alert
+					onClose={handleCloseAlert}
+					severity={severity}
+					sx={{ width: "100%" }}
+					>
+					{messageRetour}
+					</Alert>
+				</Snackbar>
+			</div>
+		</div>
+	</div>
   );
 }
 
